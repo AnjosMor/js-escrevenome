e5644ec24974cc0bee678c53592ff2412aaf23cd
AnjosMor <charlesanjos@prof.educacao.sp.gov.br>
2024-06-06 13:30:58 -0300

    adicionando arquivos css e js

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+    createCanvas(400, 400);
+    background("green")
+  }
+  
+  function draw() {
+    stroke("blue");
+    fill("black");
+    
+    
+    if (mouseIsPressed) {
+      rect(mouseX, mouseY, 20, 35);
+    }
+  }
+  
+  
+  
